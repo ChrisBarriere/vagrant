16a570fd83f5c61f4549c5664820a409f51a8f9d
Christophe Barriere <christophebarriere@free.fr>
2019-06-05 10:37:05 +0200

    typo in change_connection.ps1

--- powershell/change_connection.ps1
+++ powershell/change_connection.ps1
@@ -8,9 +8,9 @@
 Enable-InternetConnectionSharing -InterfaceNameMaster $env:EXTERNAL_CONNECTION_NAME -InterfaceNameClient 'vEthernet (Vagrant)'
 
 # On reprovisionne la machine pour le changement de proxy si nécessaire
-if (vagrant status --machine-readable $env:VM_HYPERV_NAME | Select-String -Pattern ',state,off') {
-  vagrant up --provision_with ansible_local_updateproxy
-}
 if (vagrant status --machine-readable $env:VM_HYPERV_NAME | Select-String -Pattern ',state,running') {
   vagrant provision --provision_with ansible_local_updateproxy
+}
+if (vagrant status --machine-readable $env:VM_HYPERV_NAME | Select-String -Pattern ',state,off') {
+  vagrant up --provision_with ansible_local_updateproxy
 }
